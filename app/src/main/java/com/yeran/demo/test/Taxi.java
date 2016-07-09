4aba5a7cb019b16fa4cc9e7023d84d6ab098e73e
yeran <binggxdian@163.com>
2016-07-09 11:22:36 +0800

    2公里以内以及8公里以内以及8公里以上测试通过，暂时未考虑时间情况 2016年7月9日11:22:32 by yeran

--- app/src/main/java/com/yeran/demo/test/Taxi.java
+++ app/src/main/java/com/yeran/demo/test/Taxi.java
@@ -2,8 +2,58 @@ package com.yeran.demo.test;
 
 /**
  * Created by yeran on 2016/7/9.
+ * <p/>
+ * 注意所有计数单位为分，最后统一转为元
  */
 public class Taxi {
 
-    public static long normalPrice
+    /**
+     * 起步价6元
+     */
+    public static long firstPrice = 600;
+
+    /**
+     * 每公里0.8元
+     */
+    public static long priceOnKm = 80;
+
+
+    public static long firstPrice() {
+        return firstPrice;
+    }
+
+    public long normalPrice(int km){
+        long price=0;
+        if(km>2) {
+            price  = 80*(km-2)+600;
+        }else if(km>0)
+            price=600;
+        return price;
+    }
+
+    /**
+     * 超过8公理意外的价格
+     * */
+    public static long addPrice(int km) {
+        if (km>0) {
+            double addPrice = priceOnKm * 1.5;
+            return (long)addPrice*km;
+        }
+        else
+        return 0;
+    }
+
+    public long getPrice(int km){
+        long price=0;
+        if(km>8){
+            long normalPrice=normalPrice(8);
+            int addKm = km-8;
+            long addPrice = addPrice(addKm);
+            price=normalPrice+addPrice;
+        }else {
+            price+=normalPrice(km);
+        }
+        return price;
+    }
+
 }
---
+++ app/src/test/java/com/yeran/demo/test/TaxiTest.java
@@ -0,0 +1,47 @@
+package com.yeran.demo.test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by yeran on 2016/7/9.
+ */
+public class TaxiTest {
+    private Taxi taxi;
+
+    @Before
+    public void init(){
+        taxi=new Taxi();
+    }
+
+
+    @Test
+    public void test_normal_price() {
+        long price = taxi.normalPrice(1);
+        assertTrue(price==600);
+    }
+
+
+
+//    public long getPrice(int km){
+//        long price=0;
+//        if(km>8){
+//            long normalPrice=normalPrice(8);
+//            int addKm = km-8;
+//            long addPrice = addPrice(addKm);
+//            price=normalPrice+addPrice;
+//        }else {
+//            price+=normalPrice(km);
+//        }
+//        return price;
+//    }
+
+    @Test
+    public void test_getPrice(){
+    //   long price = getPrice(1);
+        assertEquals(taxi.getPrice(9),1200);
+    }
+}
